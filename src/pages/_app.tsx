b734801d55a419c6535aabfac03e6f8e06367617
luandstv <luantensa@gmail.com>
2021-02-28 13:58:51 -0300

    finalizado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,11 @@
+import React, { useState } from 'react'
+
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <Component {...pageProps} />
+  )
 }
 
 export default MyApp
